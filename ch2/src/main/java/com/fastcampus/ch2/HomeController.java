08705e5f0d204a9be1c82f4657633b3963962bbc
akor11lyt <akor1@naver.com>
2022-09-01 23:19:39 +0900

    Reflction API

--- ch2/src/main/java/com/fastcampus/ch2/HomeController.java
+++ ch2/src/main/java/com/fastcampus/ch2/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@Controller		// 1. 원격 호출 가능한 프로그램으로 등록
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -22,6 +22,8 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+
+	// 2. URL과 메서드 연결(맵핑, mapping)
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
