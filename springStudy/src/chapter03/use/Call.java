e2ec3cd792f7638a054f9bfd22ff538fd80e1f34
akor11lyt <akor1@naver.com>
2022-10-09 01:41:14 +0900

    DI

---
+++ springStudy/src/chapter03/use/Call.java
@@ -0,0 +1,16 @@
+package chapter03.use;
+
+import chapter03.used.AddCalc;
+import chapter03.used.Calculator;
+import chapter03.used.SubCalc;
+
+/*
+인터페이스 의존 확인
+ */
+public class Call {
+    public static void main(String[] args) {
+        Calculator calculator = new SubCalc();
+        Integer result = calculator.calc(10, 5);
+        System.out.println("계산 결과는 ("+ result + ")입니다.");
+    }
+}
